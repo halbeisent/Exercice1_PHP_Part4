bb2148683277a32321c4bc7dd75190571b4d3728
halbeisent <dev@halbeisen.fr>
2018-05-24 09:42:21 +0200

    Exercice 1 Partie 4 Terminé

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 1 Partie 4</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <p><?php
+     function trueFunction()
+     {
+       return 'True';
+     }
+     echo trueFunction();
+     ?></p>
+  </body>
+</html>
